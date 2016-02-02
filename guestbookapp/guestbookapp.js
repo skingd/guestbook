fd45ce306cd41361ac75284dd4c0cfd050376c43
skingd <sking@mail.greenriver.edu>
2016-02-02 10:20:14 -0800

    Update

--- guestbookapp/guestbookapp.js
+++ guestbookapp/guestbookapp.js
@@ -1,51 +1,61 @@
 Messages = new Mongo.Collection("messages");
 
-if (Meteor.isClient) {
-	
-	//subscribe to database
-	Meteor.subscribe("messages");
-	
-	Template.guestBook.helpers({
-		"messages": function(){
-		return Messages.find({}, {sort: {createdOn: -1}})|| {};
-			//Return all message objects, or an empty object
-			//if database is invalid
-		}
-	});
-	
-  Template.guestBook.events(
-      {
-        "submit form": function(event){
-          event.preventDefault();
-		  
-		  var messageBox = $(event.target).find('textarea[name=guestBookMessage]');
-		 
-		  
-		  var messageText = messageBox.val();
-		  
-		  var nameBox = $(event.target).find('input[name=guestName]');
-		  var nameText = nameBox.val();
-		  
-		  
-		  Messages.insert({
-			  name: nameText,
-			  message: messageText
-			  //createdOn: Date.now();
-		  });
-		  
-          nameBox.val("");
-		  messageBox.val("");
+if (!Meteor.isClient) {
+} else {
+
+    //subscribe to database
+    Meteor.subscribe("messages");
+
+    Template.guestBook.helpers({
+        "messages": function () {
+            return Messages.find({}, {sort: {createdOn: -1}}) || {};
+            //Return all message objects, or an empty object
+            //if database is invalid
         }
-      }
-  );
+    });
+
+    Template.guestBook.events(
+        {
+            "submit form": function (event) {
+                event.preventDefault();
+
+                var messageBox = $(event.target).find('textarea[name=guestBookMessage]');
+
+
+                var messageText = messageBox.val();
+
+                var nameBox = $(event.target).find('input[name=guestName]');
+                var nameText = nameBox.val();
+
+                if (nameText.length > 0 && messageText.length > 0) {
+
+                    Messages.insert({
+                        name: nameText,
+                        message: messageText
+                        //createdOn: Date.now();
+                    });
+
+                    nameBox.val("");
+                    messageBox.val("");
+                } else {
+                    if (nameBox.val == ("")) {
+
+                    }
+                    if (messageBox.val == ("")) {
+                        messageBox.classList.setAttribute('class', 'has-warning');
+                    }
+                }
+            }
+        }
+    );
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    // de to run on server at startup
   });
   
   Meteor.publish("messages", function(){
 	  return Messages.find();
   });
- }
+}
